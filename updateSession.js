272a597cd6ce0df0e004bf101b1c961c86c74d1a
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-05 02:38:23 +0530

    Create updateSession.js

---
+++ updateSession.js
@@ -0,0 +1,87 @@
+const axios = require('axios');
+const { MONGODB_URL } = require('./config');
+const { makeid } = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router();
+const pino = require("pino");
+const {
+    default: makeWASocket,
+    useMultiFileAuthState,
+    delay,
+    Browsers,
+    makeCacheableSignalKeyStore
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+};
+
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+    let sid = req.query.session;
+
+    async function getPaire() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
+            let session = makeWASocket({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                },
+                printQRInTerminal: false,
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                browser: Browsers.macOS("Safari"),
+            });
+
+            if (!session.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await session.requestPairingCode(num);
+                if (!res.headersSent) {
+                    res.json({ code });
+                }
+            }
+
+            session.ev.on('creds.update', saveCreds);
+
+            session.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+
+                if (connection === "open") {
+                    await delay(10000);
+
+                    const jsonData = await fs.promises.readFile(`${__dirname}/temp/${id}/creds.json`, 'utf-8');
+                    const { data } = await axios.post('https://api.lokiser.xyz/mongoose/session/update', {
+                        id: sid,
+                        newData: jsonData,
+                        mongoUrl: MONGODB_URL
+                    });
+                    await session.sendMessage(session.user.id, {
+                        text: data.data
+                    });
+
+                    await delay(100);
+                    await session.ws.close();
+                    removeFile('./temp/' + id); // Not asynchronous
+                    return;
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
+                    await delay(10000);
+                    getPaire();
+                }
+            });
+        } catch (err) {
+            console.log("service restarted");
+            removeFile('./temp/' + id); // Not asynchronous
+            if (!res.headersSent) {
+                res.json({ code: "Service Unavailable" });
+            }
+        }
+    }
+
+    await getPaire();
+});
+
+module.exports = router;
