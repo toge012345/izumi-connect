e2bd90c4db6a8e436501493a0259ea3e8dc6bfd0
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-05 02:36:59 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,9 +1,9 @@
-const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
-const {makeid} = require('./id');
+const axios = require('axios');
+const { MONGODB_URL } = require('./config');
+const { makeid } = require('./id');
 const express = require('express');
 const fs = require('fs');
-let router = express.Router()
+let router = express.Router();
 const pino = require("pino");
 const {
     default: makeWASocket,
@@ -13,19 +13,18 @@ const {
     makeCacheableSignalKeyStore
 } = require("@whiskeysockets/baileys");
 
-function removeFile(FilePath){
-    if(!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true })
- };
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+};
+
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function getPaire() {
-        const {
-            state,
-            saveCreds
-        } = await useMultiFileAuthState('./temp/'+id)
-     try {
+
+    async function getPaire() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
             let session = makeWASocket({
                 auth: {
                     creds: state.creds,
@@ -33,45 +32,56 @@ router.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: Browsers.macOS("Desktop"),
+                browser: Browsers.macOS("Safari"),
              });
-             if(!session.authState.creds.registered) {
+
+            if (!session.authState.creds.registered) {
                 await delay(1500);
-                        num = num.replace(/[^0-9]/g,'');
-                            const code = await session.requestPairingCode(num)
-                 if(!res.headersSent){
-                 await res.send({code});
-                     }
-                 }
-            session.ev.on('creds.update', saveCreds)
+                num = num.replace(/[^0-9]/g, '');
+                const code = await session.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            session.ev.on('creds.update', saveCreds);
+
             session.ev.on("connection.update", async (s) => {
-                const {
-                    connection,
-                    lastDisconnect
-                } = s;
+                const { connection, lastDisconnect } = s;
+
                 if (connection == "open") {
-		await delay(10000);
-                await delay(10000);
-                    const output = await pastebin.createPasteFromFile(__dirname+`/temp/${id}/creds.json`, "pastebin-js test", null, 1, "N");
-					await session.sendMessage(session.user.id, {
-						text: output.split('/')[3]
-					})
-        await delay(100);
-        await session.ws.close();
-        return await removeFile('./temp/'+id);
-            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    await delay(10000);
+
+                    const jsonData = await fs.promises.readFile(`${__dirname}/temp/${id}/creds.json`, 'utf-8');
+                    const { data } = await axios.post('https://api.lokiser.xyz/mongoose/session/create', {
+                        SessionID: 'Bot~',
+                        creds: jsonData,
+                        mongoUrl: MONGODB_URL
+                    });
+
+                    await session.sendMessage(session.user.id, {
+                        text: data.data
+                    });
+
+                    await delay(100);
+                    await session.ws.close();
+                    return await removeFile('./temp/' + id);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
                     getPaire();
                 }
             });
         } catch (err) {
             console.log("service restated");
-            await removeFile('./temp/'+id);
-         if(!res.headersSent){
-            await res.send({code:"Service Unavailable"});
-         }
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: "Service Unavailable" });
+            }
         }
     }
-    return await getPaire()
+
+    return await getPaire();
 });
-module.exports = router
+
+module.exports = router;
