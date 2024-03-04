43f750b807e5e2585d2f0a3c241b2595b279c841
Aromal <136228489+Loki-Xer@users.noreply.github.com>
2024-03-05 02:35:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const express = require('express');
-const app = express();
-__path = process.cwd()
+const path = require('path');
 const bodyParser = require("body-parser");
+
+const app = express();
+const __path = process.cwd();
 const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/qr', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`
-	██████╗ ███████╗███████╗████████╗ █████╗ ██████╗ ██╗
-	██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗██║
-	██████╔╝█████╗  ███████╗   ██║   ███████║██████╔╝██║
-	██╔══██╗██╔══╝  ╚════██║   ██║   ██╔══██║██╔═══╝ ██║
-	██║  ██║███████╗███████║   ██║   ██║  ██║██║     ██║
-	╚═╝  ╚═╝╚══════╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝     ╚═╝
-			          
- Server running on http://localhost:` + PORT)
-})
 
-module.exports = app
+app.use(express.static(path.join(__path, 'public')));
+
+app.get('/pair', (req, res) => {
+    res.sendFile(path.join(__path, '/public/pair.html'));
+});
+
+app.get('/update-session', (req, res) => {
+    res.sendFile(path.join(__path, '/public/updateSession.html'));
+});
+
+let updateSession = require('./updateSession');
+app.use('/sid', updateSession);
+
+let qr = require('./qr');
+app.use('/qr', qr);
+
+let pair = require('./pair');
+app.use('/code', pair);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__path, '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Server running on http://localhost:' + PORT);
+});
+
+module.exports = app;
