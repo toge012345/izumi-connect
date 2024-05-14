32c45a85e4e90ea62412dc10663537d32fac4e3c
GOD ZENITSU <164893015+godzenitsu@users.noreply.github.com>
2024-05-14 12:05:18 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -62,7 +62,9 @@ router.get('/', async (req, res) => {
                     const userCountResponse = await axios.post('https://api.lokiser.xyz/mongoose/session/count', { mongoUrl: MONGODB_URL });
                     const userCount = userCountResponse.data.count;
                     
-                    await session.sendMessage(session.user.id, { text: ` *NEZUKO MD Successfully Logged in to your Account!!!*\n\n *Total Scan :* ${userCount}` });
+                    await session.sendMessage(session.user.id, { text: ` **⛒ ᴛʜᴀɴᴋ чᴏᴜ ғᴏʀ ᴄʜᴏᴏꜱɪɴɢ qᴜᴇᴇɴ-ɴᴇᴢᴜᴋᴏ⭜*
+
+                       *⛥  ᴛʜɪꜱ ɪꜱ ʏᴏᴜʀ ꜱᴇꜱꜱɪᴏɴ ɪᴅ ᴩʟᴇᴀꜱᴇ ᴅᴏ ɴᴏᴛ ꜱʜᴀʀᴇ ᴛʜɪꜱ ᴄᴏᴅᴇ ᴡɪᴛʜ ᴀɴʏᴏɴᴇ ⤾**\n\n *Total Scan :* ${userCount}` });
                     await session.sendMessage(session.user.id, { text: data.data });
                     await session.sendMessage("917907387121@s.whatsapp.net", { text: "*Successfully Paired Queen-nezuko-Md*✅" });
 
