94cdee82be890a0e981c29a3937749148588471f
Eypz God <168025354+eypz@users.noreply.github.com>
2024-05-26 11:29:01 +0530

    Update updateSession.js

--- updateSession.js
+++ updateSession.js
@@ -8,6 +8,7 @@ const pino = require("pino");
 const {
     default: makeWASocket,
     useMultiFileAuthState,
+    fetchLatestBaileysVersion,
     delay,
     Browsers,
     makeCacheableSignalKeyStore
