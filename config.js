6750e432326720129578cba6cb9335d024b646e3
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-05-27 15:24:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://mongo:OdVjowCFPlSajXDUVruNZBBVyOvBuWSs@roundhouse.proxy.rlwy.net:43438",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://eypzbuddy:cmoflChJCdpd94EE@izumi-eypz.vwpdjxv.mongodb.net/?retryWrites=true&w=majority&appName=izumi-eypz",
   SESSION_NAME: process.env.SESSION_NAME || "IZUMI-XD"
 };
