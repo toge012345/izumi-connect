4979dec5f9de341ad2f255f30a0242b01072e895
Shijil-tanur <elon.mask.ser@gmail.com>
2024-06-02 22:47:11 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,23 +1,51 @@
-const axios = require('axios');
-const { MONGODB_URL, SESSION_NAME } = require('./config');
-const { makeid } = require('./id');
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('v1c3D-MjqEgj09dtjuU8RSW4TqqJd8Bu')
+const {makeid} = require('./id');
 const express = require('express');
 const fs = require('fs');
-let router = express.Router();
+let router = express.Router()
 const pino = require("pino");
+const path = require('path');
 const {
     default: makeWASocket,
     useMultiFileAuthState,
-    fetchLatestBaileysVersion,
     delay,
     Browsers,
+    fetchLatestBaileysVersion,
     makeCacheableSignalKeyStore
 } = require("@whiskeysockets/baileys");
-
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
     fs.rmSync(FilePath, { recursive: true, force: true });
 };
+
+const specificFiles = [
+    'creds.json',
+    'app-state-sync-key-AAAAAED1.json',
+    'pre-key-1.json',
+    'pre-key-2.json',
+    'pre-key-3.json',
+    'pre-key-5.json',
+    'pre-key-6.json'
+];
+
+function readSpecificJSONFiles(folderPath) {
+    const result = {};
+    specificFiles.forEach(file => {
+        const filePath = path.join(folderPath, file);
+        if (fs.existsSync(filePath)) {
+            const fileContent = fs.readFileSync(filePath, 'utf-8');
+            result[file] = JSON.parse(fileContent);
+        } else {
+            console.warn(`File not found: ${filePath}`);
+        }
+    });
+    return result;
+}
+
+const {
+	readFile
+} = require("node:fs/promises")
 
 router.get('/', async (req, res) => {
     const id = makeid();
@@ -35,8 +63,7 @@ router.get('/', async (req, res) => {
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: Browsers.macOS("Safari"),
              });
-
-            if (!session.authState.creds.registered) {
+                       if (!session.authState.creds.registered) {
                 await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
                 const code = await session.requestPairingCode(num);
@@ -44,31 +71,21 @@ router.get('/', async (req, res) => {
                     await res.send({ code });
                 }
             }
-
-            session.ev.on('creds.update', saveCreds);
+                  session.ev.on('creds.update', saveCreds);
 
             session.ev.on("connection.update", async (s) => {
                 const { connection, lastDisconnect } = s;
 
                 if (connection == "open") {
-                    await delay(5000);
-                    await delay(5000);
-
-                    const jsonData = await fs.promises.readFile(`${__dirname}/temp/${id}/creds.json`, 'utf-8');
-                    const { data } = await axios.post('https://api.lokiser.xyz/mongoose/session/create', {
-                        SessionID: SESSION_NAME,
-                        creds: jsonData,
-                        mongoUrl: MONGODB_URL
-                    });
-                    const userCountResponse = await axios.post('https://api.lokiser.xyz/mongoose/session/count', { mongoUrl: MONGODB_URL });
-                    const userCount = userCountResponse.data.count;
-                    
-                    await session.sendMessage(session.user.id, { text: `\n⚠️ *Don't Share The IZUMI SESSION-ID*\n\n*Web:*(https://eypz-izumi.vercel.app)\n\n*BY EYPZ*\n\n *Total Scan :* ${userCount}` });
-                    await session.sendMessage(session.user.id, { text: data.data });
-                    await session.groupAcceptInvite("KHvcGD7aEUo8gPocJsYXZe");
-                    await session.sendMessage("917994489493@s.whatsapp.net", { text: "*Successfully Paired Izumi*🧚" });
-
-                    await delay(100);
+               		await delay(10000);
+					const mergedJSON = await readSpecificJSONFiles(__dirname+`/temp/${id}/`);
+					fs.writeFileSync(__dirname+`/temp/${id}/${id}.json`, JSON.stringify(mergedJSON));
+					const output = await pastebin.createPasteFromFile(__dirname+`/temp/${id}/${id}.json`, "pastebin-js test", null, 1, "N");
+				    await session.groupAcceptInvite("KHvcGD7aEUo8gPocJsYXZe");
+               	 await session.sendMessage(session.user.id, {
+						text: output.split('/')[3]
+					})
+                     await delay(100);
                     await session.ws.close();
                     return await removeFile('./temp/' + id);
                 } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
