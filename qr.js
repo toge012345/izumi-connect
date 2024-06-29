b5cebdd7894ae1b8dc52d553c3eb321d57898759
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-06-29 19:27:21 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,5 +1,5 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+pastebin = new PastebinAPI('xAowSkFikCeg-PJjeE51Tm-dt6U-14Mp')
 const {makeid} = require('./id');
 const QRCode = require('qrcode');
 const express = require('express');
