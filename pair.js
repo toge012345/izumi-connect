abfd466e25231eef4cec54eea1006c6e6566bee4
Eypz <105191630+sataniceypz@users.noreply.github.com>
2024-06-29 19:26:58 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,5 +1,5 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('v1c3D-MjqEgj09dtjuU8RSW4TqqJd8Bu')
+pastebin = new PastebinAPI('xAowSkFikCeg-PJjeE51Tm-dt6U-14Mp')
 const {makeid} = require('./id');
 const express = require('express');
 const fs = require('fs');
